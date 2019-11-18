35a3446eb005c2d57f1ccbb04ac625882c8cde95
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-11-18 13:48:20 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -8,9 +8,12 @@ Config	= {}
 
 Config.RespawnTime = 30000
 Config.LocaleDead = "Nie żyjesz"
-Config.LocaleTimer = "Odrodzenie dostępne za "
+Config.LocaleTimer = "Odrodzenie dostępne za"
+Config.Timecycle = "CrossLine01" --Set the timecycle effect for a death
+Config.TimecycleStrenght = 1.0
+
+--- DEV COMMANDS
+
 Config.RespawnCommand = true
 Config.kysCommand = true
 Config.Coords = true
-Config.Timecycle = "CrossLine01"
-Config.TimecycleStrenght = 1.0
