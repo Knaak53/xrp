1618c0cf2c766c26a3e8d20583352d41c2e4867d
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-11-18 19:56:16 +0100

    Update cl_main.lua

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -2,3 +2,20 @@
 -- Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License --
 
 print(_serverPrefix .. "Version: " .. _VERSION .. " LOADED!")
+
+function printClient(message)
+    print(_serverPrefix .. message)
+end
+
+local firstSpawn = false
+
+Citizen.CreateThread(function()
+    while firstSpawn == false do
+        local spawned = Citizen.InvokeNative(0xB8DFD30D6973E135 --[[NetworkIsPlayerActive]], PlayerPedId(), Citizen.ResultAsInteger())
+        if spawned then
+            printClient("Player spawned!")
+            TriggerServerEvent("xrp:firstSpawn")
+            firstSpawn = true
+        end
+    end
+end)
