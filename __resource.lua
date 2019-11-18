046d286a30035d6c827e2554675ecde9339cdabf
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-11-18 19:39:42 +0100

    major changes

--- __resource.lua
+++ __resource.lua
@@ -1,12 +1,12 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_scripts {
-    'client/client.lua',
 	'config.lua',
 	'spawnpoints.lua',
+	'client/cl_functions.lua',
 	'client/cl_horse.lua',
 }
 
 server_scripts {
-	'server/server.lua',
+	'server/sv_functions.lua',
 }
---
+++ server/sv_functions.lua
@@ -0,0 +1,95 @@
+AddEventHandler('xrp_db:doesUserExist', function(identifier, callback)
+	MySQL.Async.fetchAll('SELECT 1 FROM users WHERE `identifier`=@identifier;', {identifier = identifier}, function(users)
+		if users[1] then
+			callback(true)
+		else
+			callback(false)
+		end
+	end)
+end)
+
+AddEventHandler('xrp_db:retrieveUser', function(identifier, callback)
+	local Callback = callback
+	MySQL.Async.fetchAll('SELECT * FROM users WHERE `identifier`=@identifier;', {identifier = identifier}, function(users)
+		if users[1] then
+			Callback(users[1])
+		else
+			Callback(false)
+		end
+	end)
+end)
+
+AddEventHandler('xrp_db:createUser', function(identifier, license, cash, gold, callback)
+	local user = {
+		identifier = identifier,
+		money = cash or 0,
+		gold = gold or 0,
+		license = license,
+		group = 'user'
+	}
+
+	MySQL.Async.execute('INSERT INTO users (`identifier`, `money`, `gold`, `group`, `license`) VALUES (@identifier, @money, @gold, @group, @license);',
+	{
+		identifier = user.identifier,
+		money = user.money,
+		gold = user.gold,
+		group = user.group,
+		license = user.license
+	}, function(rowsChanged)
+		callback()
+	end)
+end)
+
+AddEventHandler('xrp_db:retrieveLicensedUser', function(license, callback)
+	MySQL.Async.fetchAll('SELECT * FROM users WHERE `license`=@license;', {license = license}, function(users)
+		if users[1] then
+			callback(users[1])
+		else
+			callback(false)
+		end
+	end)
+end)
+
+AddEventHandler('xrp_db:doesLicensedUserExist', function(license, callback)
+	MySQL.Async.fetchAll('SELECT 1 FROM users WHERE `license`=@license;', {license = license}, function(users)
+		if users[1] then
+			callback(true)
+		else
+			callback(false)
+		end
+	end)
+end)
+
+AddEventHandler('xrp_db:updateUser', function(identifier, new, callback)
+	Citizen.CreateThread(function()
+		local updateString = ''
+		local params = {identifier = identifier}
+
+		local length = tLength(new)
+		local cLength = 1
+		for k,v in pairs(new) do
+			if (type(k) == 'string') then
+				updateString = updateString .. '`' .. k .. '`=@' .. k
+				params[k] = v
+				if cLength < length then
+					updateString = updateString .. ', '
+				end
+			end
+			cLength = cLength + 1
+		end
+
+		MySQL.Async.execute('UPDATE users SET ' .. updateString .. ' WHERE `identifier`=@identifier', params, function(rowsChanged)
+			if callback then
+				callback(true)
+			end
+		end)
+	end)
+end)
+
+function tLength(t)
+	local l = 0
+	for k,v in pairs(t)do
+		l = l + 1
+	end
+	return l
+end
