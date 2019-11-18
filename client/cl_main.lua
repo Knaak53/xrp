bf05befd950b4097befce59cf1041a9b10a60e74
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-11-18 19:53:19 +0100

    Prefix add

---
+++ client/cl_main.lua
@@ -0,0 +1,4 @@
+--                                     Licensed under                                     --
+-- Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License --
+
+print(_serverPrefix .. "Version: " .. _VERSION .. " LOADED!")
---
+++ server/sv_main.lua
@@ -0,0 +1,7 @@
+--                                     Licensed under                                     --
+-- Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License --
+
+_serverPrefix = "XRP: "
+_VERSION = 'PRE-LAUNCH 0.1'
+_firstCheckPerformed = false
+_UUID = LoadResourceFile(GetCurrentResourceName(), "uuid") or "unknown"
