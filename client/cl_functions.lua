2684a726ff9b6833b89b7023a8f7ee217469152c
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-11-21 18:00:12 +0100

    added Heading in ShowCoords

--- client/cl_functions.lua
+++ client/cl_functions.lua
@@ -34,8 +34,8 @@ Citizen.Wait(0) -- DO NOT REMOVE
 		Citizen.InvokeNative(0xFA08722A5EA82DA7, Config.Timecycle)
 		Citizen.InvokeNative(0xFDB74C9CC54C3F37, Config.TimecycleStrenght)
 		Citizen.InvokeNative(0x405224591DF02025, 0.50, 0.475, 1.0, 0.22, 1, 1, 1, 100, true, true)
-		DrawTxt(Config.LocaleDead, 0.50, 0.40, 1.0, 1.0, true, 161, 3, 0)
-		DrawTxt(Config.LocaleTimer .. " " .. tonumber(string.format("%.0f", (((GetGameTimer() - timer) * -1)/1000))), 0.50, 0.50, 0.7, 0.7, true, 255, 255, 255) 
+		DrawTxt(Config.LocaleDead, 0.50, 0.40, 1.0, 1.0, true, 161, 3, 0, 255, true)
+		DrawTxt(Config.LocaleTimer .. " " .. tonumber(string.format("%.0f", (((GetGameTimer() - timer) * -1)/1000))), 0.50, 0.50, 0.7, 0.7, true, 255, 255, 255, 255, true) 
 			--print ("PLAYER IS DEAD")
 				end
 				--Citizen.InvokeNative(0x40C719A5E410B9E4, 1) -- FADE OUT    //   BROKEN AT THIS MOMENT 18/11/2019
@@ -52,7 +52,8 @@ Citizen.Wait(0)
 if Config.Coords then
 local ent = GetPlayerPed(source)
 local pp = GetEntityCoords(ent)
-DrawTxt("x = " .. tonumber(string.format("%.2f", pp.x)) .. " y = " .. tonumber(string.format("%.2f", pp.y)) .. " z = " .. tonumber(string.format("%.2f", pp.z)), 0.50, 0.0, 0.4, 0.4, true, 255, 255, 255)
+local hd = GetEntityHeading(ent)
+DrawTxt("x = " .. tonumber(string.format("%.2f", pp.x)) .. " y = " .. tonumber(string.format("%.2f", pp.y)) .. " z = " .. tonumber(string.format("%.2f", pp.z)) .. " | H: " .. tonumber(string.format("%.2f", hd)), 0.01, 0.0, 0.4, 0.4, true, 255, 255, 255, 150, false)
 else end
 end
 end)
@@ -67,14 +68,14 @@ function respawn(source)
 end
 
 --=============================================================-- DRAW TEXT SECTION--=============================================================--
-function DrawTxt(str, x, y, w, h, enableShadow, col1, col2, col3)
+function DrawTxt(str, x, y, w, h, enableShadow, col1, col2, col3, a, centre)
     local str = CreateVarString(10, "LITERAL_STRING", str)
 
 
     --Citizen.InvokeNative(0x66E0276CC5F6B9DA, 2)
     SetTextScale(w, h)
-    SetTextColor(math.floor(col1), math.floor(col2), math.floor(col3), 255)
-	SetTextCentre(true)
+    SetTextColor(math.floor(col1), math.floor(col2), math.floor(col3), math.floor(a))
+	SetTextCentre(centre)
     if enableShadow then SetTextDropshadow(1, 0, 0, 0, 255) end
     DrawText(str, x, y)
 end
