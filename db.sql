af1ca62d9d82b6e931d15d984f02938b21fbb03d
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-11-25 12:03:40 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,12 +3,16 @@ CREATE DATABASE IF NOT EXISTS `xrp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLA
 USE `xrp`;
 
 -- Dumping structure for table xrp.users
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `identifier` varchar(50) COLLATE utf8mb4_bin NOT NULL,
   `license` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `money` int(11) DEFAULT NULL,
   `gold` int(11) DEFAULT NULL,
   `group` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `firstname` varchar(12) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `lastname` varchar(12) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `xp` int(11) DEFAULT NULL,
+  `level` int(11) DEFAULT NULL,
   PRIMARY KEY (`identifier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
