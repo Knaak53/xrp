534f481c7c0cf4b237a8428ba99e5ebe94c3f45e
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-11-25 20:37:40 +0100

    Transfered respawn function to another resource

--- config.lua
+++ config.lua
@@ -2,15 +2,6 @@ Config	= {}
 
 
 
-
-
-
-
-Config.RespawnTime = 30000
-Config.LocaleDead = "Nie żyjesz"
-Config.LocaleTimer = "Odrodzenie dostępne za"
-Config.Timecycle = "CrossLine01" --Set the timecycle effect for a death
-Config.TimecycleStrenght = 1.0
 Config.ShowID = true -- true/false to set the player source ID showing on HUD
 
 --- DEV COMMANDS
