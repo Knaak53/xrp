89b7e197099151b17ea95748ef6646b64bafe6c7
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-11-25 20:43:55 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,14 +3,4 @@ Config	= {}
 
 
 Config.ShowID = true -- true/false to set the player source ID showing on HUD
-
---- DEV COMMANDS
-
-Config.RespawnCommand = true
-Config.kysCommand = true
 Config.Coords = true
-
-Config.Spawnpoints = {
-    {x=205.73,y=118.26,z=99.28},
-    {x=211.18,y=161.95,z=97.42},
-}
