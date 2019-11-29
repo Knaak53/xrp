a921dae0422da6cd5f5af57232115d8557bc6d59
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-11-29 17:48:39 +0100

    Transfered Show Coords function to main resource

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -22,5 +22,42 @@ Citizen.CreateThread(function()
 end)
 
 AddEventHandler("onClientResourceStart", function() -- Reveal whole map on spawn
-	Citizen.InvokeNative(0x4B8F743A4A6D2FF8, true)
+    if Config.RevealMap then
+    Citizen.InvokeNative(0x4B8F743A4A6D2FF8, true)
+    else end
 end)
+
+Citizen.CreateThread(function()
+    while true do
+    Citizen.Wait(0)
+        DrawCoords()
+    end
+    end)
+    
+
+function DrawCoords()
+        if Config.Coords then
+    local _source = source
+    local ent = GetPlayerPed(_source)
+    local pp = GetEntityCoords(ent)
+    local hd = GetEntityHeading(ent)
+    DrawTxt("x = " .. tonumber(string.format("%.2f", pp.x)) .. " y = " .. tonumber(string.format("%.2f", pp.y)) .. " z = " .. tonumber(string.format("%.2f", pp.z)) .. " | H: " .. tonumber(string.format("%.2f", hd)), 0.01, 0.0, 0.4, 0.4, true, 255, 255, 255, 150, false)
+    else end
+    end
+
+
+function DrawTxt(str, x, y, w, h, enableShadow, col1, col2, col3, a, centre)
+        local str = CreateVarString(10, "LITERAL_STRING", str)
+    
+    
+        --Citizen.InvokeNative(0x66E0276CC5F6B9DA, 2)
+        SetTextScale(w, h)
+        SetTextColor(math.floor(col1), math.floor(col2), math.floor(col3), math.floor(a))
+        SetTextCentre(centre)
+        if enableShadow then SetTextDropshadow(1, 0, 0, 0, 255) end
+        DrawText(str, x, y)
+    end
+    
+    function CreateVarString(p0, p1, variadic)
+        return Citizen.InvokeNative(0xFA925AC00EB830B9, p0, p1, variadic, Citizen.ResultAsLong())
+    end
--- config.lua
+++ config.lua
@@ -1,3 +1,5 @@
 Config	= {}
 Config.ShowID = true -- true/false to set the player source ID showing on HUD
 Config.AdminCommands = true -- Enable/Disable Admin Commands
+Config.RevealMap = true -- REVEAL ALL OF THE MAP
+Config.Coords = true -- SHOW COORDS
